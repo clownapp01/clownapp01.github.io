b4e2a00ad5dc5e6ece854384bdafc9f79c9a48bb
GitHub Action <119336515+clownapp01@users.noreply.github.com>
2022-11-28 17:10:09 +0000

    Update sub-web

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/js/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/js/workbox-v4.3.1"});
 
 importScripts(
-  "/js/precache-manifest.60a9511e4a5840e6a77aa5c61dca9026.js"
+  "/js/precache-manifest.62f3f842b16d9274d1a7732858eaf7fa.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sub-web"});
